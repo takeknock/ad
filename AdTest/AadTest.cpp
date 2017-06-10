4f53413a62be67bec9fe0d962ed6aadfba27bb9e
mmatthew <takeknock@gmail.com>
2017-06-10 16:55:22 +0900

    add AdTest class

---
+++ AdTest/AadTest.cpp
@@ -0,0 +1,20 @@
+#include "AadTest.h"
+
+
+namespace adtest {
+    TEST_F(AadTest, add_test) {
+
+    }
+    TEST_F(AadTest, sub_test) {
+
+    }
+
+    TEST_F(AadTest, mul_test) {
+
+    }
+
+    TEST_F(AadTest, div_test) {
+
+    }
+
+} // namespace adtest
---
+++ AdTest/AadTest.h
@@ -0,0 +1,11 @@
+#pragma once
+
+#include <gtest/gtest.h>
+#include "Ad/Aad.h"
+
+namespace adtest {
+    class AadTest: public ::testing::Test {
+
+    };
+
+} // namespace adtest 
