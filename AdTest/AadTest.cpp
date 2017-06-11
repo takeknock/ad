ee67cf3eb8b2b4f097c15d2c312e2b8296b1e393
mmatthew <takeknock@gmail.com>
2017-06-11 21:15:36 +0900

    implement add test

--- AdTest/AadTest.cpp
+++ AdTest/AadTest.cpp
@@ -5,6 +5,13 @@ namespace adtest {
     TEST_F(AadTest, add_test) {
         ad::Aad x1(1.0);
         ad::Aad x2(2.0);
+
+        auto y = x1 + x2;
+        double expected_value = 3.0;
+        
+        EXPECT_DOUBLE_EQ(expected_value, y.value());
+
+
     }
     TEST_F(AadTest, sub_test) {
 
