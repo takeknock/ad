be24c3d9c4123f3387b7dedda417807abbba72e7
mmatthew <takeknock@gmail.com>
2017-06-19 20:29:51 +0900

    add tests of operators with double

--- AdTest/AadTest.cpp
+++ AdTest/AadTest.cpp
@@ -2,7 +2,8 @@
 
 
 namespace adtest {
-    TEST_F(AadTest, add_test) {
+    TEST_F(AadTest, add_test) 
+    {
         ad::Aad x1(1.0);
         const ad::Aad x2(2.0);
 
@@ -16,7 +17,8 @@ namespace adtest {
         EXPECT_DOUBLE_EQ(1.0, y.getDerivative(x2));
     }
 
-    TEST_F(AadTest, sub_test) {
+    TEST_F(AadTest, sub_test) 
+    {
         const ad::Aad x1(1.0);
         ad::Aad x2(2.0);
 
@@ -30,7 +32,8 @@ namespace adtest {
         EXPECT_DOUBLE_EQ(1.0, y.getDerivative(x2));
     }
 
-    TEST_F(AadTest, mul_test) {
+    TEST_F(AadTest, mul_test) 
+    {
         ad::Aad x1(3.0);
         const ad::Aad x2(2.0);
 
@@ -44,7 +47,8 @@ namespace adtest {
         EXPECT_DOUBLE_EQ(3.0, y.getDerivative(x2));
     }
 
-    TEST_F(AadTest, div_test) {
+    TEST_F(AadTest, div_test) 
+    {
         ad::Aad x1(5.0);
         const ad::Aad x2(2.0);
 
@@ -56,6 +60,63 @@ namespace adtest {
         // each partial derivatives
         EXPECT_DOUBLE_EQ(0.5, y.getDerivative(x1));
         EXPECT_DOUBLE_EQ(-1.25, y.getDerivative(x2));
+    }
+
+    TEST_F(AadTest, add_test_with_double) 
+    {
+        ad::Aad x1(5.0);
+        double x2 = 2.0;
+
+        const ad::Aad y = x1 + x2;
+        double expected_value = 7.0;
+
+        // value
+        EXPECT_DOUBLE_EQ(expected_value, y.value());
+        // each partial derivatives
+        EXPECT_DOUBLE_EQ(1, y.getDerivative(x1));
+    }
+
+    TEST_F(AadTest, sub_test_with_double)
+    {
+        ad::Aad x1(5.0);
+        double x2 = 2.0;
+
+        const ad::Aad y = x1 - x2;
+        double expected_value = 3.0;
+
+        // value
+        EXPECT_DOUBLE_EQ(expected_value, y.value());
+        // each partial derivatives
+        EXPECT_DOUBLE_EQ(1, y.getDerivative(x1));
+    }
+
+    TEST_F(AadTest, mul_test_with_double)
+    {
+        ad::Aad x1(5.0);
+        double x2 = 2.0;
+
+        const ad::Aad y = x1 * x2;
+        double expected_value = 10.0;
+
+        // value
+        EXPECT_DOUBLE_EQ(expected_value, y.value());
+        // each partial derivatives
+        EXPECT_DOUBLE_EQ(2.0, y.getDerivative(x1));
+    }
+
+
+    TEST_F(AadTest, div_test_with_double)
+    {
+        ad::Aad x1(5.0);
+        double x2 = 2.0;
+
+        const ad::Aad y = x1 / x2;
+        double expected_value = 2.5;
+
+        // value
+        EXPECT_DOUBLE_EQ(expected_value, y.value());
+        // each partial derivatives
+        EXPECT_DOUBLE_EQ(0.5, y.getDerivative(x1));
     }
 
 } // namespace adtest
