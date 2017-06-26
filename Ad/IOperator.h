9ac8bbec70465d4c6a7ede0be0057f69742104df
mmatthew <takeknock@gmail.com>
2017-06-26 22:20:30 +0900

    update interface of diff function of operators

--- Ad/IOperator.h
+++ Ad/IOperator.h
@@ -1,9 +1,16 @@
 #pragma once
+#include <memory>
+
+#include "Ad/fwd.h"
+
 
 namespace ad {
     class IOperator {
     public:
-        EXPORT_API virtual double diff() = 0;
+        EXPORT_API virtual void diff(
+            std::shared_ptr<ComputationalGraph> parent,
+            std::shared_ptr<ComputationalGraph> left,
+            std::shared_ptr<ComputationalGraph> right) = 0;
 
         virtual ~IOperator() {};
     };
