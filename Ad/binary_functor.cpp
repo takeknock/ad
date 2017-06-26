01a035f0cd524e3107df193ade42d6929929c698
mmatthew <takeknock@gmail.com>
2017-06-26 22:46:49 +0900

    implement sub operator

--- Ad/binary_functor.cpp
+++ Ad/binary_functor.cpp
@@ -17,7 +17,10 @@ namespace ad {
         std::shared_ptr<ComputationalGraph> left,
         std::shared_ptr<ComputationalGraph> right)
     {
-
+        double nextLeft = left->getLeft()->getDerivative() - parent->getDerivative();
+        left->getLeft()->setDerivative(nextLeft);
+        double nextRight = right->getRight()->getDerivative() - parent->getDerivative();
+        right->getRight()->setDerivative(nextRight);
     }
 
     void op_mul::diff(
@@ -25,7 +28,10 @@ namespace ad {
         std::shared_ptr<ComputationalGraph> left,
         std::shared_ptr<ComputationalGraph> right)
     {
-
+        double nextLeft = left->getLeft()->getDerivative() - parent->getDerivative();
+        left->getLeft()->setDerivative(nextLeft);
+        double nextRight = right->getRight()->getDerivative() - parent->getDerivative();
+        right->getRight()->setDerivative(nextRight);
     }
 
     void op_div::diff(
